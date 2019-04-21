c3642e9d1887274cd10ca0a928bef3e01aa4c65a
Lucas Morris <emperorcow@gmail.com>
2019-04-21 14:51:54 -0400

    Debugging WinRM

--- scanners/winrm/winrm.go
+++ scanners/winrm/winrm.go
@@ -2,6 +2,7 @@ package winrm
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"strconv"
 	"strings"
@@ -40,7 +41,7 @@ func (this winrmScanner) SupportedAuthenticationExample() map[string]string {
 }
 
 // Gets out credentials
-func (this winrmScanner) Prepare(creds []scanners.Credential, cmd string, outChan chan scanners.Result) error {
+func (this *winrmScanner) Prepare(creds []scanners.Credential, cmd string, outChan chan scanners.Result) error {
 	// Load out output channel and command
 	this.out = outChan
 	this.cmd = cmd
@@ -51,6 +52,7 @@ func (this winrmScanner) Prepare(creds []scanners.Credential, cmd string, outCha
 // Runs the actual scan, takes an input of our target, the creds we need to use for this one,
 // a command to run if we have one, and our out channel for results
 func (this winrmScanner) Scan(target string) {
+	fmt.Printf("--- SCAN start\n")
 	// Add port if the user dosn't provide. Default for basic auth winrm is 5985
 	if !strings.Contains(target, ":") {
 		target = target + ":5985"
@@ -58,14 +60,18 @@ func (this winrmScanner) Scan(target string) {
 
 	// Vars to hold our winrmScanner connection and any erros
 	for _, cred := range this.creds {
+		fmt.Printf("--- CRED: %+v\n", cred)
+
 		var err error
-		var connection *winrm.Client
+		var client *winrm.Client
 
 		// Depending on the authentication type, run the correct connection function
 		switch cred.Type {
 		case "basic":
-			_, connection, err = this.basicConnect(cred.Account, target, cred.AuthData)
+			client, err = this.basicConnect(cred.Account, target, cred.AuthData)
 		}
+
+		fmt.Printf("--- CLIENT: %+v\n", client)
 
 		// Let's assume we connect succesfully
 		result := scanners.Result{
@@ -75,80 +81,90 @@ func (this winrmScanner) Scan(target string) {
 			Output:  "",
 		}
 
-		// If we got an error let's set it
+		// Create a shell on the object, making a connection to the system
+		shell, err := client.CreateShell()
+		fmt.Printf("--- SHELL: %+v\n", shell)
 		if err != nil {
 			result.Message = err.Error()
 			result.Status = false
+		} else {
+			defer shell.Close() // We'll be good and close our connection when done
 		}
 
 		// if we didn't get an error and we have a command ot run, let's do it.
 		if err == nil && this.cmd != "" {
+			fmt.Printf("--- CMD: %s\n", this.cmd)
 			// Execute the command
-			result.Output, err = this.executeCommand(this.cmd, connection)
+			result.Output, err = this.executeCommand(this.cmd, shell)
 			if err != nil {
 				// If we got an error let's let the user know
 				result.Output = "Script Error: " + err.Error()
 			}
+		} else {
+			fmt.Printf("--- CMD: NONE RUN\n")
 		}
+
+		fmt.Printf("--- RES: %+v\n\n", result)
 
 		// Finally pass results to the outChan
 		this.out <- result
 	}
+	fmt.Printf("--- SCAN done\n")
 }
 
 // Executes a command on a winrm client connection, returns an error if there is one
-func (this winrmScanner) executeCommand(cmd string, connection *winrm.Client) (string, error) {
-	//Runs the command in the client
-	output := new(bytes.Buffer)
-	rpipe, wpipe := io.Pipe()
+func (this winrmScanner) executeCommand(cmd string, shell *winrm.Shell) (string, error) {
+	// Execute our command on the connection we have.
+	command, err := shell.Execute(cmd)
+	if err != nil {
+		return "", err
+	}
 
-	wg := new(sync.WaitGroup)
-	finchan := make(chan bool, 1)
-	errchan := make(chan error, 1)
+	// We need some buffers for STDOUT and STDERR
+	var outWriter, errWriter bytes.Buffer
 
-	wg.Add(1)
-
+	// This waitgroup is to sync up the output buffers routines
+	var wg sync.WaitGroup
+	// We have two waits, one each for STDOUT and STDERR
+	wg.Add(2)
 	go func() {
 		defer wg.Done()
-		_, err := connection.Run(cmd, wpipe, wpipe)
-		if err != nil {
-			errchan <- err
-			return
-		}
-
-		finchan <- true
+		io.Copy(&outWriter, command.Stdout)
 	}()
-
-	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		io.Copy(output, rpipe)
+		io.Copy(&errWriter, command.Stderr)
 	}()
 
+	// We'll wait for the command to finish and both buffer routines to finish before we move on.
+	command.Wait()
 	wg.Wait()
 
-	select {
-	case err := <-errchan:
-		return "", err
-	case <-finchan:
-		return output.String(), nil
-	}
+	// Let's get the strings from our buffers and join them
+	output := outWriter.String() + errWriter.String()
+
+	// Finally we can close up and return
+	return output, err
 }
 
-func (this winrmScanner) basicConnect(user, host, pass string) (*winrm.Endpoint, *winrm.Client, error) {
+// This function builds out a WinRM Client struct for us to then use to actually connect later.
+func (this winrmScanner) basicConnect(user, host, pass string) (*winrm.Client, error) {
 	// Split the host into host/ip and port
 	tz := strings.Split(host, ":")
-	tzh, tzp := tz[0], tz[1]
-	tzpi, _ := strconv.Atoi(tzp)
-	// tzh for host and tzp for port (tzpi is the int type of the port), default winrm is 5985 or 5986
-	endpoint := winrm.NewEndpoint(tzh, tzpi, false, false, nil, nil, nil, 0)
-	// auth to the endpoint
+	// Get our two variables
+	tzHost, tzPort := tz[0], tz[1]
+	// Convert to the port into an integer
+	tzPortInt, _ := strconv.Atoi(tzPort)
+	// Create a new endpoint struct with our port: NewEndpoint(host string, port int, https bool, insecure bool, Cacert, cert, key []byte, timeout time.Duration)
+	endpoint := winrm.NewEndpoint(tzHost, tzPortInt, false, false, nil, nil, nil, 0)
+
+	// Build our auth to the object, does not connect yet.
 	client, err := winrm.NewClient(endpoint, user, pass)
 
-	return endpoint, client, err
+	return client, err
 }
 
 // Create a new scanner
 func NewScanner() scanners.Scanner {
-	return winrmScanner{}
+	return &winrmScanner{}
 }
