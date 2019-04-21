06bc949acdcc24db484e3bb7a792343207c9ba04
Dan Borges <ahhh.db@gmail.com>
2019-04-21 02:59:09 -0400

    updating main

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/emperorcow/go-netscan/scanners"
 	"github.com/emperorcow/go-netscan/scanners/ssh"
+	"github.com/emperorcow/go-netscan/scanners/winrm"
 )
 
 // A channel to hold our input data.  It will be one target string per line
@@ -215,14 +216,18 @@ func runScanners(scanner scanners.Scanner, creds []scanners.Credential, exec str
 	// running.
 	runDoneWait.Add(1)
 
+	// Prepare our data in the scanner before we start running scans
+	err := scanner.Prepare(creds, exec, outChan)
+	if err != nil {
+		return
+	}
+
 	for {
 		select {
 		//In the event we have a target, let's process it.
 		case target := <-inChan:
-			for _, cred := range creds {
-				scanner.Scan(target, cred, exec, outChan)
-				// LOW PRIORITY: Add forced timeouts to scans
-			}
+			scanner.Scan(target)
+			// TODO: Add forced timeouts to scans
 
 		// We'll use doneChan to signal that the program is complete (probably out of input).
 		// When we get data on this channel as a signal, we'll signal that this routine is done
@@ -240,6 +245,7 @@ func setupScanners() map[string]scanners.Scanner {
 	var scanners map[string]scanners.Scanner
 
 	scanners["ssh"] = ssh.NewScanner()
+	scanners["winrm"] = winrm.NewScanner()
 
 	return scanners
 }
